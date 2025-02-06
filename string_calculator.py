4846c8812f1148261e740288d719e3f76f975c0e
hiteshNath-Dev <hiteshnathsysorex@gmail.com>
2025-02-06 22:39:47 +0530

    feat: Introdue add function signature

---
+++ string_calculator.py
@@ -0,0 +1,12 @@
+def add(numbers: str) -> int:
+    
+    """
+    Adds numbers from a given string.
+
+    Args:
+        numbers (str): A string containing numbers seperated by delimiters.
+
+    Returns:
+        int: The sum of the numbers. Returns o if the string is empty.
+    """
+    return 0
